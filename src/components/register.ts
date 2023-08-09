cfc17f113088d776000e4245922accb0cc93b7fb
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-08-10 04:47:34 +0530

    wip

--- src/components/register.ts
+++ src/components/register.ts
@@ -1,12 +1,22 @@
 "use server";
 import { TrackClient, RegionUS } from "customerio-node";
+import axios from "axios";
 
 export const register = async (email: string) => {
   const cio = new TrackClient(process.env.SITE_ID!, process.env.API_KEY!, {
     region: RegionUS,
   });
 
-  await cio.identify(5, {
-    email,
-  });
+  await cio.identify(email);
+  await axios.post(
+    "https://track.customer.io/api/v1/32segments/%7Bsegment_id%7D/add_customers",
+    {
+      ids: [email],
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY}`,
+      },
+    }
+  );
 };
