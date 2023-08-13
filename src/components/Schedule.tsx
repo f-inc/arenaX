9737a3f10965ae2a81fe62eb0c8e1d81e1311744
Ibrahim Ahmed <abeahmed2@gmail.com>
2023-08-12 20:02:20 -0700

    Use proper HTML -- huge amount of missing <span> elements and text in divs

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,6 +1,9 @@
 export const Schedule = () => {
   return (
-    <div id="schedule" className="w-full flex flex-col items-center justify-center px-5 lg:px-0">
+    <div
+      id="schedule"
+      className="w-full flex flex-col items-center justify-center px-5 lg:px-0"
+    >
       <img
         className="w-80 lg:w-[22rem] h-20"
         src="/images/schedule.png"
@@ -11,80 +14,82 @@ export const Schedule = () => {
           Day 0 - Tuesday 8/22
         </div>
         <div className="text-[#ffc700] bg-[#08151B] text-center rounded-2xl text-lg ibm-400 uppercase px-10 py-6 lg:mt-10 info-gradient">
-          <span className="text-[#878787]"> 5:00 PM - 6:30 PM: </span> Hackathon
-          Kickoff and Team Forming Mixer
+          <span className="text-[#878787]"> 5:00 PM - 6:30 PM: </span>
+          <span>Hackathon Kickoff and Team Forming Mixer</span>
         </div>
       </div>
       <div className="flex flex-col items-center lg:my-20 text-center">
         <div className="hero-gradient text-white text-2xl lg:text-4xl orbitron font-bold mt-12 my-8 lg:mt-0 lg:my-0">
-          Day 1 - Wednesday 8/23
+          <span>Day 1 - Wednesday 8/23</span>
         </div>
         <div className="flex justify-center items-center gap-3 self-stretch flex-wrap lg:mt-10 lg:mx-60 2xl:mx-80">
           <div className="text-[#ffc700] bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 9:00 AM: </span>{" "}
+            <span className="text-[#878787]"> 9:00 AM: </span>
+            <span> </span>
             <span className="text-white">
               {" "}
               Doors Open (coffee + breakfast provided)
             </span>
           </div>
           <div className="text-[#ffc700] bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 11:00AM - 1:00PM: </span>{" "}
-            Technical Workshops
+            <span className="text-[#878787]"> 11:00AM - 1:00PM: </span>
+            <span> Technical Workshops</span>
           </div>
           <div className="text-white bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 12:30 PM: </span>Lunch
+            <span className="text-[#878787]"> 12:30 PM: </span>
+            <span>Lunch</span>
           </div>
           <div className="text-white bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 9:00 PM:</span> Doors Close Close
+            <span className="text-[#878787]"> 9:00 PM:</span>{" "}
+            <span>Doors Close Close</span>
           </div>
         </div>
       </div>
 
       <div className="flex flex-col items-center lg:my-20 text-center">
         <div className="hero-gradient text-white text-2xl lg:text-4xl orbitron font-bold mt-12 my-8 lg:mt-0 lg:my-0">
-          Day 2 - Thursday 8/24
+          <span>Day 2 - Thursday 8/24</span>
         </div>
         <div className="flex justify-center items-center gap-3 self-stretch flex-wrap lg:mt-10 lg:mx-28 2xl:mx-60">
           <div className="text-white bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 9:00 AM: </span> Doors Open
+            <span className="text-[#878787]"> 9:00 AM: </span>
+            <span> Doors Open</span>
           </div>
           <div className="text-white bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 11:00 PM: </span> Midpoint
-            Check-in
+            <span className="text-[#878787]"> 11:00 PM: </span>
+            <span> Midpoint Check-in</span>
           </div>
           <div className="text-white bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]">12:30 PM: </span> Lunch
+            <span className="text-[#878787]">12:30 PM: </span>
+            <span> Lunch</span>
           </div>
           <div className="text-[#ffc700] bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 2:00 PM:</span> Basketball
-            Challenge hosted by Warriors
+            <span className="text-[#878787]"> 2:00 PM:</span>
+            <span> Basketball Challenge hosted by Warriors</span>
           </div>
           <div className="text-white bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 7:00 PM: </span> Doors Close
+            <span className="text-[#878787]"> 7:00 PM: </span>
+            <span>Doors Close</span>
           </div>
         </div>
       </div>
       <div className="flex flex-col items-center lg:my-20 text-center">
         <div className="hero-gradient text-white text-2xl lg:text-4xl orbitron font-bold mt-12 my-8 lg:mt-0 lg:my-0">
-          Day 3 - Friday 8/25{" "}
+          <span>Day 3 - Friday 8/25 </span>
         </div>
         <div className="flex justify-center items-center gap-3 self-stretch flex-wrap lg:mt-10 lg:mx-32 2xl:mx-60">
           <div className="text-white bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]">9:00 AM: </span> Doors Open
+            <span className="text-[#878787]">9:00 AM: </span>
+            <span> Doors Open</span>
           </div>
           <div className="text-white bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 12:30 PM: </span> Lunch
+            <span className="text-[#878787]"> 12:30 PM: </span>
+            <span> Lunch</span>
           </div>
           <div className="text-[#ffc700] bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]">3:00 PM: </span> Submissions + Judging
+            <span className="text-[#878787]">3:00 PM: </span>
+            <span>Submissions + Judging</span>
           </div>
-          {/* <div className="text-[#ffc700] bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 6:00 PM: </span> Demos and Judging
-          </div>
-          <div className="text-[#ffc700] bg-[#08151B] rounded-2xl text-xl ibm-400 uppercase px-10 py-6 info-gradient">
-            <span className="text-[#878787]"> 7:00 PM:</span> Awards and Closing
-            Ceremony
-          </div> */}
         </div>
       </div>
     </div>
