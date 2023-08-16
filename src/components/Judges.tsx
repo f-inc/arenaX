2062e47fc7eb673b82afa62d06d4517d534f8096
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2023-08-16 07:51:48 +0530

    update judges

--- src/components/Judges.tsx
+++ src/components/Judges.tsx
@@ -3,6 +3,7 @@ import Zaza from "images/judges/zaza.png";
 import Furqan from "images/judges/furqan.png";
 import Suman from "images/judges/suman.png";
 import Ivan from "images/judges/ivan.png";
+import Vishal from "images/judges/vishal.png";
 
 const judges = [
   {
@@ -25,6 +26,11 @@ const judges = [
     role: "Founder, Cerebral Valley",
     imageSrc: Ivan,
   },
+  {
+    name: "Vishal",
+    role: "Founding Partner of StonebriarX",
+    imageSrc: Vishal,
+  },
 ];
 
 export const Judges = () => {
@@ -33,13 +39,14 @@ export const Judges = () => {
       id="partners"
       className="w-full flex flex-col justify-center items-center pb-20 lg:py-20"
     >
-      <img className="w-72 h-20" src="/images/judges.png" alt="" />
-      <div className="grid grid-cols-1 gap-y-12 lg:grid-cols-4 gap-x-10 mt-12 lg:mt-20">
+      <img className="w-72 h-20" src="/images/judges.png" alt="Judges" />
+
+      <div className="flex flex-wrap justify-center gap-y-12 lg:gap-x-10 mt-12 lg:mt-20">
         {judges.map((judge, index) => (
-          <div key={index} className="flex flex-col">
-            <div className="w-60 h-60">
+          <div key={index} className="flex flex-col mb-12 lg:mb-0">
+            <div className="rounded-xl w-60 h-60 overflow-hidden">
               <Image
-                className="rounded-xl"
+                className="object-cover"
                 src={judge.imageSrc}
                 width={240}
                 height={240}
